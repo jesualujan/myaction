4deac1d9b887c69340fb3ac826adf326275be32e
jesualujan <lujan.oficial@gmail.com>
2024-02-13 15:11:39 -0600

    ✨ simple application

---
+++ app.js
@@ -0,0 +1,6 @@
+// A JavaScript date library for parsing, validating, manipulating, and formatting dates.
+const moment = require('moment')
+console.log("HELLO, DEMO ACTIONS! ")
+const date = moment().format('MMMM Do YYYY, h:mm:ss a')
+console.log(date)
+
